71cf0ce0485c285224986e2c0d91c3ad3fe9932b
cloudsony999 <cloudsony999@gmail.com>
2022-05-30 12:26:39 +0530

    first commit

---
+++ myproj/src/demo/Test.java
@@ -0,0 +1,14 @@
+package demo;
+
+import entity.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student(1, "samik");
+		System.out.println(s.getName());
+
+	}
+
+}
